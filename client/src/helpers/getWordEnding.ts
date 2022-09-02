eca189e44ccc6f08f82e44297f68866c92265e5b
igor <mafiestko@ya.ru>
2022-09-02 18:12:09 +0300

    Добавляет возможность удалять слова из словарей. Добавляет функцию хелпер для подбора правильных окончаний к слову в зависимости от числа

---
+++ client/src/helpers/getWordEnding.ts
@@ -0,0 +1,19 @@
+export const getWordEnding = (number: number | string, one: string, two: string, five: string) => {
+	if (!Number.isFinite(number)) {
+		console.log(`Вы передаёте: ${number} = ${typeof number} передайте корректное число`)
+		return
+	}
+	let n = Math.abs(+number)
+	n %= 100
+	if (n >= 5 && n <= 20) {
+		return five
+	}
+	n %= 10
+	if (n === 1) {
+		return one
+	}
+	if (n >= 2 && n <= 4) {
+		return two
+	}
+	return five
+}
