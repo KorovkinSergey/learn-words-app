3c0dcb409531f84c8dc5cbe32f156c302ff3a029
SergeiKorovkin <98321980+SergeiKorovkin@users.noreply.github.com>
2022-12-17 18:47:06 +0300

    fix bug

--- app.ts
+++ app.ts
@@ -1,42 +1,39 @@
-import express from "express";
-import config from "config";
+import express from 'express'
+import config from 'config'
 
-import auth from "./src/routes/auth.routes";
-import path from "path";
-import dictionary from "./src/routes/dictionary.routes";
+import auth from './src/routes/auth.routes'
+import path from 'path'
+import dictionary from './src/routes/dictionary.routes'
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const app = express();
+const app = express()
 
-app.use(express.json({}));
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb' }))
 
-app.use("/api/dictionary", dictionary);
-app.use("/api/auth", auth);
+app.use('/api/dictionary', dictionary)
+app.use('/api/auth', auth)
 
-if (process.env.NODE_ENV === "production") {
-	app.use("/", express.static(path.join(__dirname, "client", "build")));
+if (process.env.NODE_ENV === 'production') {
+	app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
 }
 
-const PORT = config.get("port") || 5005;
+const PORT = config.get('port') || 5005
 
 async function start() {
 	try {
-		await mongoose.connect(config.get("mongoURL"));
+		await mongoose.connect(config.get('mongoURL'))
 
-		app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+		app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
 	} catch (e: any) {
-		console.log("Server error: ", e.message);
-		process.exit(1);
+		console.log('Server error: ', e.message)
+		process.exit(1)
 	}
-
-
 }
 
-start();
-
-
+start()
